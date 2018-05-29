ff4a8908bf24a123f9a1654cd581d899f7065481
Joe Fitter <joefitter@hotmail.com>
2018-05-29 11:13:21 +0100

    simplified tests

--- test/unit/permissions/index.spec.js
+++ test/unit/permissions/index.spec.js
@@ -10,79 +10,68 @@ describe('can', () => {
     };
   });
 
-  test('Rejects with an error if task is unknown', done => {
+  test('Rejects with an error if task is unknown', () => {
     const task = 'unknown.task';
     const expected = 'Unknown task: unknown.task';
-    can(user, task)
+    return can(user, task)
       .catch(err => {
         expect(err.message).toBe(expected);
-        done();
       });
   });
 
-  test('Inspector can view all establishments', done => {
+  test('Inspector can view all establishments', () => {
     const task = 'establishment.list';
     user.is.mockReturnValueOnce(true);
-    can(user, task)
+    return can(user, task)
       .then(authorised => {
         expect(user.is).toBeCalledWith('inspector');
         expect(authorised).toBe(true);
-        done();
-      })
-      .catch(done);
+      });
   });
 
-  test('Inspector can view any establishment', done => {
+  test('Inspector can view any establishment', () => {
     const task = 'establishment.read';
     const params = { id: '123' };
     user.is.mockReturnValueOnce(true);
     user.get.mockReturnValueOnce('321');
-    can(user, task, params)
+    return can(user, task, params)
       .then(authorised => {
         expect(user.is).toBeCalledWith('inspector');
         expect(authorised).toBe(true);
-        done();
-      })
-      .catch(done);
+      });
   });
 
-  test('User cannot view all establishments', done => {
+  test('User cannot view all establishments', () => {
     const task = 'establishment.list';
     user.is.mockReturnValueOnce(false);
-    can(user, task)
+    return can(user, task)
       .then(authorised => {
         expect(user.is).toBeCalledWith('inspector');
         expect(authorised).toBe(false);
-        done();
-      })
-      .catch(done);
+      });
   });
 
-  test('User cannot view an establishment if they are not associated', done => {
+  test('User cannot view an establishment if they are not associated', () => {
     const task = 'establishment.read';
     const params = { id: '123' };
     user.is.mockReturnValueOnce(false);
     user.get.mockReturnValueOnce('321');
-    can(user, task, params)
+    return can(user, task, params)
       .then(authorised => {
         expect(user.is).toBeCalledWith('inspector');
         expect(authorised).toBe(false);
-        done();
-      })
-      .catch(done);
+      });
   });
 
-  test('User can view their own establishment', done => {
+  test('User can view their own establishment', () => {
     const task = 'establishment.read';
     const params = { id: '123' };
     user.is.mockReturnValueOnce(false);
     user.get.mockReturnValueOnce('123');
-    can(user, task, params)
+    return can(user, task, params)
       .then(authorised => {
         expect(user.is).toBeCalledWith('inspector');
         expect(authorised).toBe(true);
-        done();
-      })
-      .catch(done);
+      });
   });
 });
