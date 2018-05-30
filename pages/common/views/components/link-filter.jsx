ee582b371dfc9121be8d0fef15d9d6340ffc5b2d
Joe Fitter <joefitter@hotmail.com>
2018-05-30 08:48:22 +0100

    Added tests for LinkFilter component
    * added component unit tests
    * onApply link filter now calls onChange with null

--- pages/common/views/components/link-filter.jsx
+++ pages/common/views/components/link-filter.jsx
@@ -9,39 +9,45 @@ const LinkFilter = ({
   formatter,
   prop
 }) => {
-  return <div className="link-filter">
-    <label>Filter by:</label>
-    <ul>
-      <li>
-        {
-          selected
-            ? <ApplyChanges
-              onApply={() => onChange(null)}
-              label="All"
-              filters={{}}
-            />
-            : <Fragment>All</Fragment>
-        }
-      </li>
-      {
-        filters.map(f => {
-          const label = formatter ? formatter(f) : f;
-          if (f === selected) {
-            return <li key={ f }>{ label }</li>;
+  return (
+    <div className="link-filter">
+      <label>Filter by:</label>
+      <ul>
+        <li>
+          {
+            selected
+              ? <ApplyChanges
+                onApply={() => onChange(null)}
+                label="All"
+                filters={{
+                  [prop]: []
+                }}
+              />
+              : <Fragment>All</Fragment>
           }
-          return <li key={ f }>
-            <ApplyChanges
-              onApply={() => onChange(f)}
-              label={ label }
-              filters={{
-                [prop]: [f]
-              }}
-            />
-          </li>;
-        })
-      }
-    </ul>
-  </div>;
+        </li>
+        {
+          filters.map(f => {
+            const label = formatter ? formatter(f) : f;
+            if (f === selected) {
+              return <li key={ f }>{ label }</li>;
+            }
+            return (
+              <li key={ f }>
+                <ApplyChanges
+                  onApply={() => onChange(f)}
+                  label={ label }
+                  filters={{
+                    [prop]: [f]
+                  }}
+                />
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
 };
 
 export default LinkFilter;
