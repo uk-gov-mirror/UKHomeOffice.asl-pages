b297aa8e5cc90db4937777fedb89a6c4b4d1d655
Leonard Martin <leonard.martin@gmail.com>
2018-06-12 13:23:55 +0100

    Remove `where` param from get nacwos API call

--- pages/place/middleware/get-roles.js
+++ pages/place/middleware/get-roles.js
@@ -1,5 +1,5 @@
 module.exports = req => {
-  return req.api(`/establishment/${req.establishment}/roles?where%5Btype%5D=nacwo`)
+  return req.api(`/establishment/${req.establishment}/roles`, { query: { type: 'nacwo' } })
     .then(response => Promise.resolve(response.json.data))
     .catch(err => Promise.reject(err));
 };
