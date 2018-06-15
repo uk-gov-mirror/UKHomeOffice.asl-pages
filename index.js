3e03146d240611176fd26308883c2205adae95a3
Leonard Martin <leonard.martin@gmail.com>
2018-06-15 17:05:46 +0100

    Remove api and ui app bootstrapping code
    Export only pages related content as the default export - including a middleware to mount the default content, and a pat to the views directory to be used in implementing applications.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const path = require('path');
+const { merge } = require('lodash');
+const content = require('./pages/common/content');
+
 module.exports = {
-  ui: require('./ui'),
-  api: require('./api')
+  pages: require('./pages'),
+  content: (req, res, next) => {
+    res.locals.static = res.locals.static || {};
+    res.locals.static.content = merge({}, content, res.locals.static.content);
+    next();
+  },
+  views: path.resolve(__dirname, './pages/common/views')
 };
