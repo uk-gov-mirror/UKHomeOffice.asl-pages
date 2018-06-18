1c24be6cf3b869aae4205ff8d17b68bd51a9270e
Joe Fitter <joefitter@hotmail.com>
2018-06-18 13:29:04 +0100

    added functional tests for place, place/edit and place/edit/confirm

--- test/functional/fixtures/index.js
+++ test/functional/fixtures/index.js
@@ -1,13 +1,22 @@
 const fixtures = [
   {
-    url: /^\/establishment\/[a-z0-9]+$/,
+    url: /^\/establishments?\/[a-z0-9]+$/,
     response: require('./establishment.json')
+  },
+  {
+    url: /^\/establishments?\/[a-z0-9]+\/places?\/[a-z0-9]+$/,
+    response: require('./place.json')
+  },
+  {
+    url: /^\/establishments?\/[a-z0-9]+\/roles?$/,
+    response: require('./nacwos.json')
   }
 ];
 
 module.exports = url => {
+  console.log(url);
   const data = fixtures.reduce((r, fixture) => {
-    return !r && url.match(fixture.url) && fixture.response;
+    return r || (url.match(fixture.url) && fixture.response);
   }, null);
 
   return Promise.resolve({ json: { data } });
