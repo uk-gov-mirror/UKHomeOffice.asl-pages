377a458cebdaaa523dc9d04ddb5ac69e210b1bca
Joe Fitter <joefitter@hotmail.com>
2018-07-02 14:50:44 +0100

    updated content paths

--- pages/establishment/dashboard/content/index.js
+++ pages/establishment/dashboard/content/index.js
@@ -1,16 +1,24 @@
 module.exports = {
   dashboard: {
-    details: {
-      subtitle: 'Contact information for this establishment, as well as any conditions and authorisations.'
+    establishment: {
+      details: {
+        subtitle: 'Contact information for this establishment, as well as any conditions and authorisations.'
+      }
     },
-    people: {
-      subtitle: 'Named people, licence holders, and the Home Office Liason Contact.'
+    profile: {
+      list: {
+        subtitle: 'Named people, licence holders, and the Home Office Liason Contact.'
+      }
     },
-    places: {
-      subtitle: 'Approved areas in this establishment where animals may be used.'
+    place: {
+      list: {
+        subtitle: 'Approved areas in this establishment where animals may be used.'
+      }
     },
-    projects: {
-      subtitle: 'Projects with primary availability at this establishment.'
+    project: {
+      list: {
+        subtitle: 'Projects with primary availability at this establishment.'
+      }
     }
   }
 };
