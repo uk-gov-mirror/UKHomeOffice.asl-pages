51c9269953e9351b73bb332526a57fa03c1b23db
Leonard Martin <leonard.martin@gmail.com>
2018-07-03 18:06:07 +0100

    Fix test for people page

--- pages/profile/list/tests/index.js
+++ pages/profile/list/tests/index.js
@@ -4,6 +4,6 @@ describe('Profile List', () => {
   it('can load', () => {
     browser.url('/pages/profile/list');
     const title = browser.getText('h1');
-    assert.equal(title, 'Named people and licence holders');
+    assert.equal(title, 'People');
   });
 });
