ae977d968d75e586a7a6d1e096fed92201b1c1dd
Joe Fitter <joefitter@hotmail.com>
2018-07-10 10:39:04 +0100

    simplified standard comparator

--- pages/common/views/components/diff.jsx
+++ pages/common/views/components/diff.jsx
@@ -10,7 +10,7 @@ const getValue = (key, { format } = {}) => {
 const Diff = ({
   diff,
   formatters = {},
-  comparator = (...args) => !(isEqual(args[1], args[2])),
+  comparator = (a, b) => !(isEqual(a, b)),
   currentLabel = 'Current',
   proposedLabel = 'Proposed'
 }) => (
