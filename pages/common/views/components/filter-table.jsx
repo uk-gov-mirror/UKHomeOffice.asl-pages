ecfeac729418b5ee74e8c7a59c478b869bf76742
Leonard Martin <leonard.martin@gmail.com>
2018-07-10 12:12:42 +0100

    Remove export links from tables
    They don't work, they can go back again when/if they do work or there are requirements.

--- pages/common/views/components/filter-table.jsx
+++ pages/common/views/components/filter-table.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import DataTable from '../containers/datatable';
 import Filters from '../containers/filters';
-import ExportLink from '../containers/export-link';
 import FilterSummary from '../containers/filter-summary';
 import Link from '../containers/link';
 import Snippet from '../containers/snippet';
@@ -20,7 +19,6 @@ const FilterTable = ({
       }
     </div>
     <DataTable formatters={ formatters } ExpandableRow={ExpandableRow} />
-    <ExportLink />
   </Fragment>
 );
 
