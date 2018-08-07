2c96060b416b755b2d88b61bec1c36012d06b88a
Joe Fitter <joefitter@hotmail.com>
2018-08-07 13:22:29 +0100

    revert status bar change

--- pages/common/views/components/status-bar.jsx
+++ pages/common/views/components/status-bar.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
-import Link from '../components/link';
+import Link from '../containers/link';
 
 const StatusBar = ({
   user: { profile },
   logoutLink = '/logout',
-  logoutLabel = 'Sign Out',
-  urls
-}) => {
-  return (
-    <div className="status-bar">
-      {
-        profile && <React.Fragment><span><Link urls={urls} page="profile.view" profile={profile.id} label={`${profile.name}`} /></span>|</React.Fragment>
-      }
-      <span><a href={logoutLink}>{logoutLabel}</a></span>
-    </div>
-  )
-
-}
+  logoutLabel = 'Sign Out'
+}) => (
+  <div className="status-bar">
+    {
+      profile && <React.Fragment><span><Link page="profile.view" profile={profile.id} label={`${profile.name}`} /></span>|</React.Fragment>
+    }
+    <span><a href={logoutLink}>{logoutLabel}</a></span>
+  </div>
+);
 
 export default StatusBar;
