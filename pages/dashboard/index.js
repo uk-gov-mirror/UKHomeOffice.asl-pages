356923baee7054206965cbb67b28a667bbe18c1e
Elvin Ali <ali.elvin@gmail.com>
2018-08-21 11:18:24 +0100

    Add dashboard page

---
+++ pages/dashboard/index.js
@@ -0,0 +1,15 @@
+const page = require('../../lib/page');
+
+module.exports = settings => {
+  const app = page({
+    ...settings,
+    root: __dirname
+  });
+
+  app.get('/', (req, res, next) => {
+    res.locals.static.profile = req.user.profile;
+    next();
+  });
+
+  return app;
+};
