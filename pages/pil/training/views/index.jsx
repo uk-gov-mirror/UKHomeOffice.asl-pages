4806f33d91561a19e754c2e251b85de579cb32bf
Elvin Ali <ali.elvin@gmail.com>
2018-09-07 12:03:01 +0100

    Add 'Which modules did this certificate cover' page

--- pages/pil/training/views/index.jsx
+++ pages/pil/training/views/index.jsx
@@ -3,20 +3,8 @@ import { connect } from 'react-redux';
 import Snippet from '../../../common/views/containers/snippet';
 import FormLayout from '../../../common/views/layouts/form';
 
-const formatters = {
-  role: {
-    mapOptions: opt => {
-      return {
-        value: opt,
-        label: <Snippet>{`fields.role.options.${opt}.label`}</Snippet>,
-        hint: <Snippet optional>{`fields.role.options.${opt}.hint`}</Snippet>
-      };
-    }
-  }
-};
-
 const Page = () => (
-  <FormLayout formatters={formatters}>
+  <FormLayout>
     <header>
       <br />
       <h1><Snippet>pil.training.title</Snippet></h1>
