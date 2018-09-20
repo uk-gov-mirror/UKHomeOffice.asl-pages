2d3669f62c437d5ef91aca492c04d2d25164fe94
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-09-20 12:55:38 +0100

    fix the styling and links on PIL cat selection page

--- pages/dashboard/views/index.jsx
+++ pages/dashboard/views/index.jsx
@@ -21,6 +21,7 @@ const Index = ({
             establishments.map(est =>
               <li key={est}>
                 <Link page="establishment.dashboard" establishment={ est.id } label={ est.name } />
+                <br />
                 <Link page="profile.invite" establishment={ est.id } label={<Snippet>pages.dashboard.invite</Snippet>}/>
               </li>
             )
