ac51126028c2b2220caec5ac1395fb7b8f3b7521
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-10-16 12:40:29 +0100

    added dummy success page for demo

---
+++ pages/pil/application/content/success.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pil: {
+    submitted: {
+      title: 'Application submitted to NTCO',
+      summary: 'A confirmation email has been sent to ',
+      whatNext: {
+        title: 'What happens next?',
+        summary: `You are not licenced to carry out any new procedures until your licence application has been approved
+          by ASRU.`
+      },
+      body: `The Training and Competency Officer at your establishment is responsible for endorsing your training and
+        exemptions. They may contact you to ask for more information.
+
+        Exemptions may also be assessed by an inspector.`,
+      homepage: 'Homepage'
+    }
+  }
+};
