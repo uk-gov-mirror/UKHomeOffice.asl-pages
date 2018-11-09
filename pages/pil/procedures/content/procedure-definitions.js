b0447c6dbc76c35a60b2bb44b339d5909efcd3e0
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-11-09 14:34:56 +0000

    move procedureDefinitions into constants so it can be shared

--- pages/pil/procedures/content/procedure-definitions.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  A: `Minor / minimally invasive procedures not requiring sedation, analgesia or general anaesthesia.`,
-  B: `Minor / minimally invasive procedures involving sedation, analgesia or brief general anaesthesia. Plus surgical
-      procedures conducted under brief non-recovery general anaesthesia`,
-  C: `Surgical procedures involving general anaesthesia. Plus - administration and maintenance of balanced or prolonged
-      general anaesthesia.`,
-  D: `Use of neromuscular blocking agents`,
-  F: 'Other'
-};
