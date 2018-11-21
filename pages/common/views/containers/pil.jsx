879ef36accc43d451ddae59724432dd1f14cd54f
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-11-21 15:00:19 +0000

    removed unused component for now

--- pages/common/views/containers/pil.jsx
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-import Pil from '../components/pil';
-
-const mapStateToProps = ({ static: { pil, profile } }) => ({ pil, profile });
-
-export default connect(mapStateToProps)(Pil);
