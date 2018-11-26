7263d185299204cf3953cd1c7ab36a8d64ecb1f9
Joe Fitter <joefitter@hotmail.com>
2018-11-26 11:57:09 +0000

    lose breadcrumbs on invitations page

--- pages/profile/invitations/index.js
+++ pages/profile/invitations/index.js
@@ -1,23 +1,11 @@
 const page = require('../../../lib/page');
 const datatable = require('../../common/routers/datatable');
-const { crumbs } = require('@asl/service/ui/middleware');
 const schema = require('./schema');
 
 module.exports = settings => {
   const app = page({
     ...settings,
     root: __dirname
-  });
-
-  app.use((req, res, next) => {
-    const href = req.buildRoute('profile.list');
-    crumbs([
-      {
-        href,
-        label: '{{static.content.pages.profile.list}}'
-      },
-      '{{static.content.pages.profile.invitations}}'
-    ])(req, res, next);
   });
 
   app.use(datatable({
