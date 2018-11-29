8ea3fe268f39d29f6153417466892a76d9804388
Elvin Ali <ali.elvin@gmail.com>
2018-11-29 08:19:14 +0000

    Add remove permission button

--- pages/profile/permission/index.js
+++ pages/profile/permission/index.js
@@ -1,8 +1,6 @@
-// const { set } = require('lodash');
 const page = require('../../../lib/page');
 const form = require('../../common/routers/form');
 const schema = require('./schema');
-// const { crumbs } = require('@asl/service/ui/middleware');
 
 module.exports = settings => {
   const app = page({
@@ -12,18 +10,12 @@ module.exports = settings => {
 
   app.use('/', form({ schema }));
 
-  // app.use(crumbs([
-  //   { href: '{{{static.urls.profile.}}}', label: res.locals.static.establishment.name },
-  //   { href: '{{{static.urls.profile.list}}}', label: '{{static.content.pages.profile.list}}' },
-  //   { href: '{{{static.urls.profile.}}}', label: res.locals.static.profile.name },
-  //   '{{static.content.pages.profile.permission}}'
-  // ]));
-
   app.post('/', (req, res, next) => {
+
     const values = {
       role: req.session.form[req.model.id].values.permission,
-      establishmentId: res.locals.static.establishmentId,
-      profileId: res.locals.static.profileId
+      establishmentId: req.establishmentId,
+      profileId: req.profileId
     };
 
     const opts = {
@@ -33,7 +25,24 @@ module.exports = settings => {
     };
 
     return req.api(`/establishment/${req.establishmentId}/profile/${req.profileId}/permission`, opts)
-      .then(() => next())
+      .then(() => res.redirect(req.buildRoute('profile.view')))
+      .catch(next);
+  });
+
+  app.post('/remove', (req, res, next) => {
+
+    const values = {
+      profileId: req.profileId
+    };
+
+    const opts = {
+      method: 'DELETE',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(values)
+    };
+
+    return req.api(`/establishment/${req.establishmentId}/profile/${req.profileId}/permission`, opts)
+      .then(() => res.redirect(req.buildRoute('profile.list')))
       .catch(next);
   });
 
--- pages/profile/permission/views/index.jsx
+++ pages/profile/permission/views/index.jsx
@@ -1,10 +1,6 @@
 import React, { Fragment } from 'react';
-
-import {
-  Link,
-  Snippet,
-  FormLayout
-} from '@asl/components';
+import { connect } from 'react-redux';
+import { Link, Snippet, FormLayout, ApplyChanges } from '@asl/components';
 
 const formatters = {
   permission: {
@@ -12,30 +8,47 @@ const formatters = {
       return {
         value: opt,
         label: <Snippet>{`fields.permission.options.${opt}.label`}</Snippet>,
-        hint: <Snippet optional>{`fields.permission.options.${opt}.hint`}</Snippet>
+        hint: (
+          <Snippet optional>{`fields.permission.options.${opt}.hint`}</Snippet>
+        )
       };
     }
   }
 };
 
-const Page = () => (
-  <Fragment>
-    <FormLayout formatters={formatters}>
-      <header>
-        <h1><Snippet>title</Snippet></h1>
-      </header>
-    </FormLayout>
+const Page = ({ url }) => {
+  return (
     <Fragment>
-      <p><h2><Snippet>remove</Snippet></h2></p>
-      <p><Snippet>copy</Snippet></p>
-      <Link
-        page=''
-        className="govuk-button"
-        label={<Snippet>buttons.remove</Snippet>}
-      />
-      <p><Link page="profile.view" label={<Snippet>pages.profile.links.back</Snippet>} /></p>
+      <FormLayout formatters={formatters}>
+        <header>
+          <h1>
+            <Snippet>title</Snippet>
+          </h1>
+        </header>
+      </FormLayout>
+      <Fragment>
+        <h2>
+          <Snippet>remove</Snippet>
+        </h2>
+        <p>
+          <Snippet>copy</Snippet>
+        </p>
+        <ApplyChanges type="form" method="POST" action={`${url}/remove`}>
+          <button className="govuk-button">
+            <Snippet>buttons.remove</Snippet>
+          </button>
+        </ApplyChanges>
+        <p>
+          <Link
+            page="profile.view"
+            label={<Snippet>pages.profile.links.back</Snippet>}
+          />
+        </p>
+      </Fragment>
     </Fragment>
-  </Fragment>
-);
+  );
+};
 
-export default Page;
+const mapStateToProps = ({ static: { url } }) => ({ url });
+
+export default connect(mapStateToProps)(Page);
