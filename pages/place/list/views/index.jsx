6a8e73e9fa6827ab4a4c267d5c817f46942d5167
Leonard Martin <leonard.martin@gmail.com>
2018-12-03 12:42:36 +0000

    Hide edit controls for non-admin users
    Apply a filter on `place.update` permissions when showing the edit controls on a row of the schedule of premises.

--- pages/place/list/views/index.jsx
+++ pages/place/list/views/index.jsx
@@ -1,16 +1,15 @@
 import React, { Fragment } from 'react';
 import get from 'lodash/get';
 import { connect } from 'react-redux';
-import classnames from 'classnames';
-import ReactMarkdown from 'react-markdown';
 import {
   FilterTable,
   Snippet,
-  Controls,
   Link,
   Header
 } from '@asl/components';
 import formatters from '../../formatters';
+
+import ExpandableRow from './row';
 
 const pageFormatters = {
   name: {
@@ -32,42 +31,30 @@ const pageFormatters = {
   }
 };
 
-const ExpandableRow = ({ row, schema }) => (
-  <div className="govuk-grid-row">
-    <div className={classnames({
-      'govuk-grid-column-one-quarter': row.restrictions,
-      'govuk-grid-column-full': !row.restrictions
-    })}>
-      <Controls item={row.id} />
-    </div>
-    {
-      row.restrictions && (
-        <div className="govuk-grid-column-two-thirds">
-          <h3>{<Snippet>fields.changesToRestrictions.label</Snippet>}</h3>
-          <ReactMarkdown>{row.restrictions}</ReactMarkdown>
-        </div>
-      )
-    }
-  </div>
-);
-
 const Places = ({
   establishment: { name },
   allowedActions,
   ...props
-}) => (
-  <Fragment>
+}) => {
+  const expands = row => {
+    if (!allowedActions.includes('place.update') && !row.restrictions) {
+      return null;
+    }
+    return <ExpandableRow row={row} />;
+  };
+
+  return <Fragment>
     <Header
       title={<Snippet>pages.place.list</Snippet>}
       subtitle={name}
     />
     <FilterTable
       formatters={Object.assign({}, formatters, pageFormatters)}
-      ExpandableRow={ExpandableRow}
+      expands={expands}
       createPath={allowedActions.includes('place.create') && 'place.create.new'}
     />
-  </Fragment>
-);
+  </Fragment>;
+};
 
 const mapStateToProps = ({ static: { establishment, allowedActions } }) => ({ establishment, allowedActions });
 
---
+++ pages/place/list/views/row.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import ReactMarkdown from 'react-markdown';
+import {
+  Snippet,
+  Controls
+} from '@asl/components';
+
+const ExpandableRow = ({ row, schema, allowedActions }) => {
+  if (!allowedActions.includes('place.update') && !row.restrictions) {
+    return null;
+  }
+  return <div className="govuk-grid-row">
+    <div className={classnames({
+      'govuk-grid-column-one-quarter': row.restrictions,
+      'govuk-grid-column-full': !row.restrictions
+    })}>
+      {
+        allowedActions.includes('place.update') && <Controls item={row.id} />
+      }
+    </div>
+    {
+      row.restrictions && (
+        <div className="govuk-grid-column-two-thirds">
+          <h3>{<Snippet>fields.changesToRestrictions.label</Snippet>}</h3>
+          <ReactMarkdown>{row.restrictions}</ReactMarkdown>
+        </div>
+      )
+    }
+  </div>;
+};
+
+const mapStateToProps = ({ static: { allowedActions } }) => ({ allowedActions });
+
+export default connect(mapStateToProps)(ExpandableRow);
