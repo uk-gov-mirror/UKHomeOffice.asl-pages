dabf1b986efe592975904e1dd8fda4c6618398eb
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-12-13 11:06:44 +0000

    change the wording on species selection

--- pages/pil/training/content/modules.js
+++ pages/pil/training/content/modules.js
@@ -9,7 +9,7 @@ module.exports = merge({}, content, {
       hint: 'Select all that apply.'
     },
     species: {
-      label: 'Please specify a species.',
+      label: 'Please specify a type of animal.',
       add: 'Add another species'
     }
   },
