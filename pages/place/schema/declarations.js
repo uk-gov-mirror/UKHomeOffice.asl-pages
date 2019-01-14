d10edec63e654adb1c1434fb894c0b8ff9988c5b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-01-14 17:43:43 +0000

    customValidate for place (doesn't work?!)

--- pages/place/schema/declarations.js
+++ pages/place/schema/declarations.js
@@ -1,14 +1,28 @@
+const { isEqual } = require('lodash');
+const { toArray } = require('../../../lib/utils');
 const content = require('../update/content/confirm');
+
+const options = [
+  {
+    label: content.declarations.declaration1,
+    value: 'declaration-1'
+  }
+];
+
+const optionValues = options.map(declaration => declaration.value);
 
 module.exports = {
   declarations: {
     inputType: 'checkboxGroup',
-    validate: ['required'],
-    options: [
-      {
-        label: content.declarations.declaration1,
-        value: 'declaration-1'
+    options,
+    nullValue: [],
+    format: toArray,
+    validate: [{
+      customValidate: (field, model) => {
+        console.log(optionValues, model.declarations); // << doesn't get called
+
+        return isEqual(optionValues, model.declarations);
       }
-    ]
+    }]
   }
 };
