9d62faee069e677db4cb81287b1afd7f53945d43
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-01-17 16:00:04 +0000

    only throw the error if profile is 404ing

--- pages/task/read/index.js
+++ pages/task/read/index.js
@@ -22,22 +22,24 @@ module.exports = settings => {
   });
 
   app.use('/', (req, res, next) => {
-    Promise.resolve()
-      .then(() => req.user.can('profile.read.all', { establishment: req.task.data.data.establishmentId }))
-      .then(canReadAllProfiles => {
-        if (!canReadAllProfiles) {
-          const content = getContent(req.task);
-          next(new UnauthorisedError(content.errors.permissions));
-        }
-      });
-
     if (req.task.data.data.profileId && req.task.data.data.establishmentId) {
       return req.api(`/establishment/${req.task.data.data.establishmentId}/profile/${req.task.data.data.profileId}`)
         .then(({ json: { data } }) => {
           req.profile = cleanModel(data);
         })
         .then(() => next())
-        .catch(next);
+        .catch(error => {
+          return Promise.resolve()
+            .then(() => req.user.can('profile.read.all', { establishment: req.task.data.data.establishmentId }))
+            .then(canReadAllProfiles => {
+              if (!canReadAllProfiles) {
+                const content = getContent(req.task);
+                next(new UnauthorisedError(content.errors.permissions));
+              } else {
+                next(error);
+              }
+            });
+        });
     }
     next();
   });
