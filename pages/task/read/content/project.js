568dd1177f4101a24d13537be3313d5cad098333
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-04-04 15:09:14 +0100

    add declarations to latest draft section of project task

--- pages/task/read/content/project.js
+++ pages/task/read/content/project.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Review PPL application',
   'sticky-nav': {
-    'submitted-version': 'Submitted draft',
+    'submitted-version': 'Latest submitted draft',
     status: 'Next steps'
   },
   fields: {
@@ -17,5 +17,10 @@ module.exports = {
   versions: {
     granted: 'View granted version',
     submitted: 'View submitted draft'
+  },
+  declarations: {
+    'pel-holder': 'Does this application have the endorsement of your primary establishment\'s PEL holder?',
+    'awerb': 'Has the application been reviewed by the AWERB of each relevant establishment?',
+    'ready-for-inspector': 'Has the applicant deemed the application ready for Inspector approval?'
   }
 };
