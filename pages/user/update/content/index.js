132115d6aaca49fe9acfb53c77b44e07d8539308
pcuk1979 <philip.cox@digital.homeoffice.gov.uk>
2019-06-17 11:50:11 +0100

    Text amends

--- pages/user/update/content/index.js
+++ pages/user/update/content/index.js
@@ -2,24 +2,24 @@ const { merge } = require('lodash');
 const baseContent = require('../../../profile/content');
 
 module.exports = merge({}, baseContent, {
-  title: 'Edit profile',
+  title: 'Edit your details',
   fields: {
     comments: {
-      label: 'Tell us why you are making this change',
-      hint: 'If you are a licence holder or named person then your changes will need to be approved by the Home Office'
+      label: 'Why are you making this change?',
+      hint: 'If you are a licence holder or named person then your changes will need to be approved by the Home Office.'
     }
   },
   errors: {
     dob: {
       required: 'Date of birth is required',
-      dateIsBefore: 'Date of birth must be in the past'
+      dateIsBefore: 'Your date of birth must be in the past.'
     }
   },
   buttons: {
     submit: 'Submit'
   },
   notifications: {
-    success: 'Profile edited',
-    pending: 'Your requested changes have been submitted to ASRU for review.'
+    success: 'Changes submitted',
+    pending: 'Your details have been sent to the Home Office for review.'
   }
 });
