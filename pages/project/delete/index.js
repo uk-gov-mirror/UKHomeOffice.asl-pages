b06e082b54ec9983effcfd327e22590953607175
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-10 17:27:20 +0100

    handle which versions to delete in resolver

--- pages/project/delete/index.js
+++ pages/project/delete/index.js
@@ -14,13 +14,7 @@ module.exports = () => {
   });
 
   app.use('/amendment', (req, res, next) => {
-    const recentDraftVersions = req.project.versions.filter(version => version.status === 'draft' && version.createdAt > req.project.granted.createdAt);
-
-    const promises = recentDraftVersions.map(version => {
-      return req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/project-versions/${version.id}`, { method: 'DELETE' });
-    });
-
-    return Promise.all(promises)
+    return req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/draft-amendments`, { method: 'DELETE' })
       .then(() => res.redirect(req.buildRoute('project.read')))
       .catch(next);
   });
