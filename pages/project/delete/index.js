68cfbb0561d76a744a75ee460ee2071fa214e9f0
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-11 15:15:50 +0100

    only listen for post requests to these routes

--- pages/project/delete/index.js
+++ pages/project/delete/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 module.exports = () => {
   const app = Router();
 
-  app.use('/draft', (req, res, next) => {
+  app.post('/draft', (req, res, next) => {
     if (req.project.status === 'inactive') {
       return req.api(`/establishment/${req.establishmentId}/project/${req.projectId}`, { method: 'DELETE' })
         .then(() => res.redirect(`${req.buildRoute('project.list')}?status=inactive`))
@@ -13,7 +13,7 @@ module.exports = () => {
     throw new Error('Active projects cannot be deleted.');
   });
 
-  app.use('/amendment', (req, res, next) => {
+  app.post('/amendment', (req, res, next) => {
     return req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/draft-amendments`, { method: 'DELETE' })
       .then(() => res.redirect(req.buildRoute('project.read')))
       .catch(next);
