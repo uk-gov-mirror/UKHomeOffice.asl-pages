fb72c5261c7025d9c5032563416c839418909133
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-11 16:26:44 +0100

    add toaster

--- pages/project/delete/index.js
+++ pages/project/delete/index.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
+const content = require('./content');
 
 module.exports = () => {
   const app = Router();
 
   app.post('/draft', (req, res, next) => {
+    res.locals.static.content = content;
+
     if (req.project.status === 'inactive') {
       return req.api(`/establishment/${req.establishmentId}/project/${req.projectId}`, { method: 'DELETE' })
+        .then(() => req.notification({ key: 'draftDiscarded' }))
         .then(() => res.redirect(`${req.buildRoute('project.list')}?status=inactive`))
         .catch(next);
     }
@@ -14,7 +18,10 @@ module.exports = () => {
   });
 
   app.post('/amendment', (req, res, next) => {
+    res.locals.static.content = content;
+
     return req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/draft-amendments`, { method: 'DELETE' })
+      .then(() => req.notification({ key: 'amendmentDiscarded' }))
       .then(() => res.redirect(req.buildRoute('project.read')))
       .catch(next);
   });
