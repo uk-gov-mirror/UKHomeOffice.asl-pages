6ecae8fabcbb52f4f2ae61b979f669dc0f0b4dc9
Joe Fitter <joefitter@hotmail.com>
2019-07-15 16:04:05 +0100

    add comment to explain the mad

--- pages/project-version/read/index.js
+++ pages/project-version/read/index.js
@@ -32,6 +32,8 @@ module.exports = settings => {
     res.locals.model = req.version;
     res.locals.static.project = req.project;
     res.locals.static.version = req.version.id;
+    // granted legacy PPLs are displayed in "read-only" mode
+    // there is no "granted view" of legacy licences
     res.locals.static.isGranted = req.project.status === 'active' &&
       req.project.granted.id === req.version.id &&
       req.project.schemaVersion > 0;
