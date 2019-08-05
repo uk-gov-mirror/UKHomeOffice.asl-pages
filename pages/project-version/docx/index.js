537258e594de15ef77a7ac279ebd62ecf236f077
Joe Fitter <joefitter@hotmail.com>
2019-08-05 11:37:22 +0100

    us image size to calculate dimensions of image

--- pages/project-version/docx/index.js
+++ pages/project-version/docx/index.js
@@ -1,11 +1,36 @@
 const { Router } = require('express');
 const { Packer } = require('@joefitter/docx');
+const imageSize = require('image-size');
 const renderer = require('@asl/projects/client/components/download-link/renderers/docx-renderer').default;
 const schema = require('@asl/projects/client/schema').default;
+
+const MAX_IMAGE_WIDTH = 600;
+const MAX_IMAGE_HEIGHT = 800;
 
 const pack = doc => {
   const packer = new Packer(doc);
   return packer.toBuffer(doc);
+};
+
+const scaleAndPreserveAspectRatio = (srcWidth, srcHeight, maxWidth, maxHeight) => {
+  const ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+  return { width: srcWidth * ratio, height: srcHeight * ratio };
+};
+
+const updateImageDimensions = node => {
+  const src = node.data.src.substring(node.data.src.indexOf(',') + 1);
+  const buffer = Buffer.from(src, 'base64');
+  let dimensions = imageSize(buffer);
+  dimensions = scaleAndPreserveAspectRatio(
+    dimensions.width,
+    dimensions.height,
+    MAX_IMAGE_WIDTH,
+    MAX_IMAGE_HEIGHT
+  );
+  node.data.width = dimensions.width;
+  node.data.height = dimensions.height;
+  node.data.src = src;
+  return node;
 };
 
 module.exports = () => {
@@ -16,7 +41,7 @@ module.exports = () => {
     const establishment = req.project.establishment;
     const sections = Object.values(schema[req.project.schemaVersion]);
 
-    renderer({ establishment }, sections, values)
+    renderer({ establishment }, sections, values, updateImageDimensions)
       .then(pack)
       .then(buffer => {
         res.attachment(`${values.title || 'Untitled project'}.docx`);
