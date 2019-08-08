647b194a8010eae385899e4576d05c0cc86b960d
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-08-08 12:11:42 +0100

    use new isOpen flag on task

--- pages/task/read/views/deadline.jsx
+++ pages/task/read/views/deadline.jsx
@@ -5,14 +5,6 @@ import { dateFormat } from '../../../../constants';
 import { formatDate } from '../../../../lib/utils';
 import moment from 'moment-business-time';
 
-const withAsru = task => [
-  'with-inspectorate',
-  'referred-to-inspector',
-  'with-licensing',
-  'inspector-recommended',
-  'inspector-rejected'
-].includes(task.status);
-
 class Deadline extends Component {
   isExtended() {
     return !!this.props.task.data.extended;
@@ -20,8 +12,7 @@ class Deadline extends Component {
 
   render() {
     const task = this.props.task;
-    const isInspector = this.props.isInspector;
-    const isExtendable = withAsru(task) && !this.isExtended();
+    const isExtendable = this.props.isInspector && task.isOpen && !this.isExtended();
 
     const submitted = task.activityLog.reduceRight((lastSubmission, activity) => {
       const status = activity.eventName.split(':').pop();
@@ -38,7 +29,7 @@ class Deadline extends Component {
 
         <h3>{ formatDate(deadline.toDate(), dateFormat.medium) }</h3>
 
-        { isInspector && isExtendable &&
+        { isExtendable &&
           <Fragment>
             <p><Snippet>deadline.hint</Snippet></p>
             <Link
