d14cc2fb743b4eaaab12e6278fce236f95d2e78a
pcuk1979 <philip.cox@digital.homeoffice.gov.uk>
2019-09-09 10:15:12 +0100

    Link destination change
    As per https://jira.digital.homeoffice.gov.uk/browse/AS-1968

--- pages/establishment/details/content/index.js
+++ pages/establishment/details/content/index.js
@@ -8,8 +8,8 @@ module.exports = {
   },
   conditions: {
     title: 'Conditions',
-    hasConditions: 'In addition to the [standard conditions of Section 2C licences](https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/193124/Project_Licence_-_Standard_Conditions.pdf):',
-    noConditions: 'The [standard conditions of Section 2C licences](https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/193124/Project_Licence_-_Standard_Conditions.pdf) apply.'
+    hasConditions: 'In addition to the [standard conditions of Section 2C licences](https://www.gov.uk/government/publications/establishment-licence-standard-conditions):',
+    noConditions: 'The [standard conditions of Section 2C licences](https://www.gov.uk/government/publications/establishment-licence-standard-conditions) apply.'
   },
   authorisations: {
     title: 'Authorisations',
