5c36f9e5c1aa37d43214501ca180a6472efd95a2
Leonard Martin <leonard.martin@gmail.com>
2019-10-24 10:32:07 +0100

    Fix fields definition

--- pages/project/update-licence-holder/schema/experience-fields.js
+++ pages/project/update-licence-holder/schema/experience-fields.js
@@ -1,7 +1,8 @@
 const { get, flattenDeep, castArray } = require('lodash');
 const schema = require('@asl/projects/client/schema/v1/experience').default;
 
-const fieldNames = flattenDeep(schema.fields.map(field => {
+const fields = schema.fields;
+const fieldNames = flattenDeep(fields.map(field => {
   return [
     field.name,
     ...(field.options || []).filter(opt => opt.reveal).map(opt => castArray(opt.reveal).map(f => f.name))
