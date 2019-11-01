74a767337f1e827bc6a9a2b52b451e16317ebe44
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-11-01 12:13:30 +0000

    use the full label

--- pages/common/content/index.js
+++ pages/common/content/index.js
@@ -118,7 +118,7 @@ module.exports = {
   licenceHolder: 'Licence holder',
   pelh: 'Establishment licence holder',
   nprc: 'Named Person Responsible for Compliance',
-  holc: 'HOLC',
+  holc: 'Home Office Liaison Contact (HOLC)',
   inspectors: 'Inspectors',
   spoc: 'Single Point of Contact (SPoC)',
   establishment: 'Establishment',
