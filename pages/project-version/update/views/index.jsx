c60f3a43f6c4326734c8abf0b9418c91e1da4a55
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-02-17 15:30:58 +0000

    rename everywhere

--- pages/project-version/update/views/index.jsx
+++ pages/project-version/update/views/index.jsx
@@ -1,11 +1,11 @@
 import React, {Fragment} from 'react';
 import { connect } from 'react-redux';
-import LicenceStatusBanner from '../../components/status-banner';
+import ProjectStatusBanner from '../../components/project-status-banner';
 
 const Project = ({ project, version, establishment }) => {
   return (<Fragment>
     <h3 className="establishment-name">{establishment.name}</h3>
-    <LicenceStatusBanner model={project} versionId={version} />
+    <ProjectStatusBanner model={project} versionId={version} />
     <div id="ppl-drafting-tool"></div>
   </Fragment>
   );
