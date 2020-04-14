b32dc4e3c18720b7aadb63adc15e4e4236fefba0
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-04-14 11:49:33 +0100

    don't bother importing simple function

--- pages/project-version/pdf/views/nts/components/retrospective-assessment.jsx
+++ pages/project-version/pdf/views/nts/components/retrospective-assessment.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import RAContent from '@asl/projects/client/constants/retrospective-assessment';
-import raApplies from '@asl/projects/client/helpers/retrospective-assessment';
 
 export default function RetrospectiveAssessment({ project }) {
-  return <p>{ raApplies(project) ? RAContent.required : RAContent.notRequired }</p>;
+  return <p>{ project.retrospectiveAssessmentRequired ? RAContent.required : RAContent.notRequired }</p>;
 }
