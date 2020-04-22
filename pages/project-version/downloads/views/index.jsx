7aaa68703c3b0dc733d58c50499fefe2aee19094
Joe Fitter <joefitter@hotmail.com>
2020-04-22 11:20:30 +0100

    oops - need moar fragment

--- pages/project-version/downloads/views/index.jsx
+++ pages/project-version/downloads/views/index.jsx
@@ -51,9 +51,11 @@ const ProjectDownloads = () => {
 
           {
             !isLegacy && (
-              <h3><Snippet>template.heading</Snippet></h3>
-              <p><Link page="projectVersion.ppl" label={<Snippet>template.link</Snippet>} /></p>
-              <p className="govuk-hint"><Snippet>template.hint</Snippet></p>
+              <Fragment>
+                <h3><Snippet>template.heading</Snippet></h3>
+                <p><Link page="projectVersion.ppl" label={<Snippet>template.link</Snippet>} /></p>
+                <p className="govuk-hint"><Snippet>template.hint</Snippet></p>
+              </Fragment>
             )
           }
 
