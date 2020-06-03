67973ba0f0b1bd99f61f4458815236bf5d227948
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-06-03 14:36:56 +0100

    check flag before making relatedTasks request

--- pages/common/middleware/related-tasks.js
+++ pages/common/middleware/related-tasks.js
@@ -1,9 +1,19 @@
+const { Router } = require('express');
 const { merge } = require('lodash');
 const datatable = require('../routers/datatable');
 const taskListSchema = require('../../task/list/schema');
 
 module.exports = getQuery => {
-  return datatable({
+  const app = Router();
+
+  app.use((req, res, next) => {
+    if (!res.locals.static.showRelatedTasks) {
+      return next('router');
+    }
+    next();
+  });
+
+  app.use(datatable({
     getApiPath: (req, res, next) => {
       let query = getQuery(req);
       query = merge(query, req.query);
@@ -15,5 +25,7 @@ module.exports = getQuery => {
       res.locals.static.workflowConnectionError = true;
       next();
     }
-  })({ schema: taskListSchema, defaultRowCount: 5 });
+  })({ schema: taskListSchema, defaultRowCount: 5 }));
+
+  return app;
 };
