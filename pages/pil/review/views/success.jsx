d4e993515d5291f813b4d7806ce289e2c120d0b5
Leonard Martin <leonard.martin@gmail.com>
2020-06-04 11:12:25 +0100

    Add link to PIL list to review success page
    If a user has permissions to view the PIL list then replace the "Home" link that lives on the success page by default with a link to the PIL list for the establishment.
    
    Also moves the success page component fully into this repo. 100% of instances of the success page in the app load `./pages/common/views/success.jsx` from this repo, which was delegating directly to `@asl/components`, but this extra abstraction doesn't seem to add any value except to make development more involved.

--- pages/pil/review/views/success.jsx
+++ pages/pil/review/views/success.jsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, Snippet } from '@asl/components';
 import Success from '../../../common/views/success';
 
-export default Success;
+export default (...props) => {
+  const allowedActions = useSelector(state => state.static.allowedActions);
+  const canViewPils = allowedActions.includes('pil.list');
+  const onwardLink = canViewPils ? <Link page="pils" label={<Snippet>breadcrumbs.pils</Snippet>} /> : null;
+
+  return <Success onwardLink={ onwardLink } />;
+};
