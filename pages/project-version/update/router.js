e09c480e6fa35b43397aa0af3a33ed0a6b92642f
Leonard Martin <leonard.martin@gmail.com>
2020-06-19 12:26:43 +0100

    Include checksum in project patch response
    Allows the client to detect remote changes when patching projects.

--- pages/project-version/update/router.js
+++ pages/project-version/update/router.js
@@ -72,7 +72,8 @@ module.exports = settings => {
       }
     };
     req.api(`/establishments/${req.establishmentId}/projects/${req.projectId}/project-versions/${req.version.id}/patch`, opts)
-      .then(({ json: { data } }) => {
+      .then(({ json: { data, meta } }) => {
+        req.version.checksum = meta.checksum;
         req.version.data = data.data;
       })
       .then(() => next())
@@ -86,7 +87,7 @@ module.exports = settings => {
   });
 
   app.put('/', getAllChanges(), (req, res) => {
-    res.json({ changes: res.locals.static.changes });
+    res.json({ changes: res.locals.static.changes, checksum: req.version.checksum });
   });
 
   app.use((req, res, next) => res.sendResponse());
