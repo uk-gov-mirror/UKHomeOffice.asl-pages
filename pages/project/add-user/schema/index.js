661fdaf19883f60be93e93a669ad963e6027bb9d
Joe Fitter <joefitter@hotmail.com>
2020-07-02 15:23:45 +0100

    Feature - autocomplete
    * Updated components with autocomplete
    * Implemented on project change licence holder and invite collab

--- pages/project/add-user/schema/index.js
+++ pages/project/add-user/schema/index.js
@@ -1,7 +1,7 @@
 module.exports = profiles => {
   return {
     profile: {
-      inputType: 'select',
+      inputType: 'autoComplete',
       options: profiles.map(p => ({ value: p.id, label: `${p.firstName} ${p.lastName}` })),
       validate: 'required'
     }
