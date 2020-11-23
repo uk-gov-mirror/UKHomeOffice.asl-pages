57408f62b2dfb8058199d11a4cc11341e95e806a
Leonard Martin <leonard.martin@gmail.com>
2020-11-23 16:12:20 +0000

    Get `fate-of-animals` from correct location
    This content has moved into NTS section

--- pages/project-version/nts/views/components/fate-of-animals.jsx
+++ pages/project-version/nts/views/components/fate-of-animals.jsx
@@ -3,7 +3,7 @@ import ReviewField from '@asl/projects/client/components/review-field';
 import schemaV1 from '@asl/projects/client/schema/v1';
 
 const getFateOfAnimalsOptions = () => {
-  return schemaV1().protocols.subsections['fate-of-animals'].fields.find(field => field.name === 'fate-of-animals').options;
+  return schemaV1().nts.subsections['fate-of-animals'].fields.find(field => field.name === 'fate-of-animals').options;
 };
 
 export default function FateOfAnimals({ version }) {
