ff9764aedf0ccd9f61b705614e6ccf1745a19e5d
Joe Fitter <joefitter@hotmail.com>
2021-03-31 18:28:11 +0200

    Feature - unsubmit rop
    * added unsubmit rop option if rop is submitted

--- pages/rops/procedures/index.js
+++ pages/rops/procedures/index.js
@@ -5,7 +5,7 @@ module.exports = () => {
   const app = Router({ mergeParams: true });
 
   app.param('procedureId', (req, res, next, procedureId) => {
-    if (procedureId === 'create') {
+    if (procedureId === 'create' || procedureId === 'unsubmit') {
       return next();
     }
     req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/rop/${req.ropId}/procedure/${procedureId}`)
@@ -13,6 +13,17 @@ module.exports = () => {
         req.procedureId = procedureId;
         req.procedure = data;
         next();
+      })
+      .catch(next);
+  });
+
+  app.post('/unsubmit', (req, res, next) => {
+    const params = {
+      method: 'POST'
+    };
+    req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/rop/${req.ropId}/unsubmit`, params)
+      .then(() => {
+        res.redirect(req.buildRoute('rops.procedures'));
       })
       .catch(next);
   });
