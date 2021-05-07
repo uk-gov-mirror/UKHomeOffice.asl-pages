6b4da71064117c252a406377e3535b0c8ef8790c
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-05-07 12:26:53 +0100

    update rop declaration wording

--- pages/rops/submit/content/index.js
+++ pages/rops/submit/content/index.js
@@ -4,7 +4,7 @@ module.exports = {
 
 * the information I have provided is correct to my knowledge
 
-I understand I may be breaking the law if I give information I know to be false or misleading.`,
+I understand it is an offence to give information I know to be false or misleading.`,
   buttons: {
     submit: 'Agree and continue'
   }
