6dac17752b495ac46ff531fbfd595d0ce2c1fc26
Joe Fitter <joefitter@hotmail.com>
2021-07-01 10:38:27 +0200

    added rops guidance sidebar

--- pages/rops/update/views/index.jsx
+++ pages/rops/update/views/index.jsx
@@ -5,12 +5,14 @@ import { Header, Snippet, FormLayout } from '@asl/components';
 import formatters from '../formatters';
 import components from './components';
 import CancelLink from '../../components/cancel-link';
+import Guidance from '../../components/guidance';
 
 export default function Step() {
   const { step, schema, project } = useSelector(state => state.static);
   const Component = components[step];
+  const showGuidance = step !== 'confirm';
   return (
-    <FormLayout formatters={pick(formatters, Object.keys(schema || {}))} cancelLink={<CancelLink />}>
+    <FormLayout formatters={pick(formatters, Object.keys(schema || {}))} cancelLink={<CancelLink />} sidebar={showGuidance && <Guidance />}>
       <Header
         title={<Snippet>title</Snippet>}
         subtitle={project.title}
