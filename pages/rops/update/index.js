9307582c70747e3cc71f775e2d6734efb17a1936
Joe Fitter <joefitter@hotmail.com>
2021-08-06 13:02:10 +0200

    Update pages/rops/update/index.js
    Co-authored-by: Leonard Martin <leonard.martin@gmail.com>

--- pages/rops/update/index.js
+++ pages/rops/update/index.js
@@ -17,7 +17,6 @@ module.exports = () => {
           data: req.multiStep.values
         }
       };
-      console.log(params.json.data);
       req.api(`/establishment/${req.establishmentId}/project/${req.projectId}/rop/${req.ropId}`, params)
         .then(() => {
           delete req.session.form[req.model.id].values;
