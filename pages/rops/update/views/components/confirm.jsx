e49ba69737ed36f58752faa47cf677802f836c24
Joe Fitter <joefitter@hotmail.com>
2021-08-19 11:22:21 +0200

    Update pages/rops/update/views/components/confirm.jsx
    Co-authored-by: Leonard Martin <leonard.martin@gmail.com>

--- pages/rops/update/views/components/confirm.jsx
+++ pages/rops/update/views/components/confirm.jsx
@@ -107,6 +107,7 @@ export default function Confirm() {
           <dd>{yn(rop.proceduresCompleted)}</dd>
 
           <dt>Only protected embryonic forms used</dt>
+          // need to reverse the boolean for postnatal animals because the phrasing of the question changes
           <dd>{yn(rop.postnatal === null ? null : !rop.postnatal)}</dd>
 
           <dt>Endangered animals used</dt>
