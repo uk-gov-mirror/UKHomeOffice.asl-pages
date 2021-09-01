999b2aa88a0d6c2105457dc185e8a8db90fe1703
Leonard Martin <leonard.martin@gmail.com>
2021-09-01 16:16:38 +0100

    Update content labels on ROPs regulatory subpurposes page

--- pages/rops/update/views/components/regulatory-subpurposes.jsx
+++ pages/rops/update/views/components/regulatory-subpurposes.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import { Snippet } from '@asl/components';
 
-export default function Endangered() {
+export default function RegulatorySubpurposes() {
   return (
     <Fragment>
       <h3><Snippet>purpose.title</Snippet></h3>
       <ul>
-        <li>Regulatory research</li>
+        <li>Regulatory use and routine production</li>
       </ul>
     </Fragment>
   );
