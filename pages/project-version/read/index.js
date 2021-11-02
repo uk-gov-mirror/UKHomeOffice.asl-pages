dd05d423bf62fa0d12f3606f9ce3c4d3ae35d370
Joe Fitter <joefitter@hotmail.com>
2021-11-02 17:09:59 +0100

    Feature - PPL preview
    * Added preview route
    * Added isDraft and isPreview flags

--- pages/project-version/read/index.js
+++ pages/project-version/read/index.js
@@ -26,13 +26,22 @@ module.exports = settings => {
     const task = get(req.project, 'openTasks[0]');
     const showComments = req.version.status !== 'granted' && !!task;
 
-    res.locals.static.basename = req.fullApplication ? req.buildRoute('projectVersion.fullApplication') : req.buildRoute('projectVersion');
+    const previewLink = req.buildRoute('projectVersion.preview');
+
+    if (req.fullApplication) {
+      res.locals.static.basename = req.buildRoute('projectVersion.fullApplication');
+    } else if (req.isPreview) {
+      res.locals.static.basename = previewLink;
+    } else {
+      res.locals.static.basename = req.buildRoute('projectVersion');
+    }
+
     res.locals.static.projectUrl = req.buildRoute('project.read');
     res.locals.static.establishment = req.project.establishment;
     res.locals.static.isActionable = get(task, 'data.data.version') === req.versionId;
     res.locals.static.user = req.user.profile;
     res.locals.static.showComments = showComments;
-    res.locals.static.commentable = showComments && req.user.profile.asruUser && res.locals.static.isCommentable;
+    res.locals.static.commentable = !req.isPreview && showComments && req.user.profile.asruUser && res.locals.static.isCommentable;
 
     const taskId = task ? task.id : null;
 
@@ -48,7 +57,8 @@ module.exports = settings => {
       ? req.version.status !== 'draft'
       : req.version.status === 'granted';
 
-    res.locals.static.editConditions = req.user.profile.asruUser &&
+    res.locals.static.editConditions = !req.isPreview &&
+      req.user.profile.asruUser &&
       task && task.withASRU &&
       req.version.status === 'submitted' &&
       req.project.versions[0].id === req.version.id;
@@ -59,8 +69,14 @@ module.exports = settings => {
 
     // granted legacy PPLs are displayed in "read-only" mode
     // there is no "granted view" of legacy licences
-    const isGranted = req.project.status === 'active' && req.version.status === 'granted' && !req.fullApplication;
+    const isGranted = req.isPreview || (req.project.status === 'active' && req.version.status === 'granted' && !req.fullApplication);
     res.locals.static.isGranted = isGranted && req.project.schemaVersion > 0;
+
+    res.locals.static.isPreview = req.isPreview && req.project.status === 'inactive';
+    res.locals.static.isDraft = req.project.status === 'inactive';
+    res.locals.static.previewLink = previewLink;
+    res.locals.static.asruUser = req.user.profile.asruUser;
+
     res.locals.static.legacyGranted = isGranted && req.project.schemaVersion === 0;
     if (task && task.data.action === 'transfer') {
       const establishments = task.data.meta.establishment;
