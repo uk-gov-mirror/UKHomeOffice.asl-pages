9afda8c3f57338911b0479b472b751d532d942e8
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-11-09 11:52:12 +0000

    remove console log

--- pages/rops/procedures/list/routers/review.js
+++ pages/rops/procedures/list/routers/review.js
@@ -3,11 +3,6 @@ const { Router } = require('express');
 module.exports = () => {
   const app = Router({ mergeParams: true });
 
-  app.get('/', (req, res, next) => {
-    console.log(req.datatable);
-    next();
-  });
-
   app.get('/', (req, res) => res.sendResponse());
 
   return app;
