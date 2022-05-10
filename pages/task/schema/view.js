f92c64ba774d786f1a76d3ed450c5c5817551442
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-05-10 18:14:42 +0100

    show a different submit label for returned role amendments

--- pages/task/schema/view.js
+++ pages/task/schema/view.js
@@ -8,6 +8,14 @@ module.exports = task => {
     .filter(step => step.id !== 'discarded-by-asru') // don't display ASRU discard as an option (has separate UI)
     .filter(step => step.id !== 'recovered') // don't display recovered as an option (has separate UI)
     .map(option => {
+      if (option.id === 'resubmitted' && task.data.model === 'role') {
+        return {
+          value: option.id,
+          label: get(content, `status.${option.id}.action.role-${task.type}`, get(content, `status.${option.id}.action`)),
+          hint: get(content, `status.${option.id}.hint.${task.type}`, get(content, `status.${option.id}.hint.default`))
+        };
+      }
+
       return {
         value: option.id,
         label: get(content, `status.${option.id}.action.${task.type}`, get(content, `status.${option.id}.action`)),
