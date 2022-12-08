9a72f9f2baf5d8f84a1582d748c043e2cd89784d
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2022-12-08 15:31:00 +0000

    Fix bug

--- pages/task/read/views/models/establishment.jsx
+++ pages/task/read/views/models/establishment.jsx
@@ -60,7 +60,7 @@ export default function Establishment({ task, values }) {
             <h2><Snippet>conditions.title</Snippet></h2>
             <Conditions
               conditions={taskData.conditions ? taskData.conditions : establishment.conditions }
-              reminders={taskData.conditions ? [JSON.parse(taskData.reminder)] : establishment.reminders}
+              reminders={taskData.reminder !== '' ? [JSON.parse(taskData.reminder)] : establishment.reminders}
               label={<Snippet>conditions.hasConditions</Snippet>}
               noConditionsLabel={<Snippet>conditions.noConditions</Snippet>}
               canUpdate={canUpdateConditions && !openTask}
