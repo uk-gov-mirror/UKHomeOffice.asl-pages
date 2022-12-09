57f08e9c5d8b99d1aaedc26b1f1d9fbd15a0e994
Garry Watts <garry.watts@bluebubblemedia.com>
2022-12-09 06:57:33 +0000

    ASL-4171 - Show details for re-useable step on protocols summary

--- pages/project-version/pdf/views/protocols.jsx
+++ pages/project-version/pdf/views/protocols.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider, useSelector } from 'react-redux';
 import { Protocol } from '@asl/projects/client/pages/sections/granted/pdf-protocols';
+import { hydrateSteps } from '@asl/projects//client/helpers/steps';
 import ProtocolConditions from '@asl/projects/client/pages/sections/protocols/protocol-conditions';
 import Review from '@asl/projects/client/components/review-field';
 import Wrapper from '../../../common/views/pdf/wrapper';
@@ -48,52 +49,55 @@ function Protocols() {
       <h3>{ title.join(' - ') }</h3>
       <p>The general constraints apply to all protocols, these are described in detail towards the end of this document.</p>
       {
-        protocols.map((protocol, index) => (
-          <Protocol
-            key={index}
-            protocol={protocol}
-            number={index + 1}
-            project={project}
-            isLegacy={false}
-            className="adverse-effects"
-          >
-            <div className="wrapper">
-              <div className="questions-grid">
-                <div>
-                  <p><strong>Where this protocol can be carried out</strong></p>
+        protocols.map((protocol, index) => {
+          const [ steps ] = hydrateSteps(project.protocols, protocol.steps || [], project.reusableSteps || {});
+          return (
+            <Protocol
+              key={index}
+              protocol={protocol}
+              number={index + 1}
+              project={project}
+              isLegacy={false}
+              className="adverse-effects"
+            >
+              <div className="wrapper">
+                <div className="questions-grid">
+                  <div>
+                    <p><strong>Where this protocol can be carried out</strong></p>
+                  </div>
+                  <div>
+                    <Review value={protocol.locations} type="location-selector"/>
+                  </div>
+                  <div>
+                    <p><strong>Typical experience or end-to-end scenario for an animal</strong></p>
+                  </div>
+                  <div>
+                    <p><Review value={protocol['experience-summary']} type="texteditor"/></p>
+                  </div>
+                  <div>
+                    <p><strong>General humane endpoints</strong></p>
+                  </div>
+                  <div>
+                    <p><Review value={protocol['experience-endpoints']} type="texteditor"/></p>
+                  </div>
                 </div>
-                <div>
-                  <Review value={protocol.locations} type="location-selector" />
-                </div>
-                <div>
-                  <p><strong>Typical experience or end-to-end scenario for an animal</strong></p>
-                </div>
-                <div>
-                  <p><Review value={protocol['experience-summary']} type="texteditor" /></p>
-                </div>
-                <div>
-                  <p><strong>General humane endpoints</strong></p>
-                </div>
-                <div>
-                  <p><Review value={protocol['experience-endpoints']} type="texteditor" /></p>
-                </div>
-              </div>
 
-              <h3 className="steps">Protocol steps</h3>
-            </div>
-            {
-              protocol.steps && protocol.steps.length
-                ? (
-                  <table>
-                    {
-                      protocol.steps.map((step, index) => <Step key={index} number={index + 1} step={step} />)
-                    }
-                  </table>
-                )
-                : <p>No steps added</p>
-            }
-          </Protocol>
-        ))
+                <h3 className="steps">Protocol steps</h3>
+              </div>
+              {
+                steps && steps.length
+                  ? (
+                    <table>
+                      {
+                        steps.map((step, index) => <Step key={index} number={index + 1} step={step}/>)
+                      }
+                    </table>
+                  )
+                  : <p>No steps added</p>
+              }
+            </Protocol>
+          );
+        })
       }
       <div className="page-break-before">
         <ProtocolConditions pdf={true} />
