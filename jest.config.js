e1ee4b303caa1b3143db94e529cbafaa99281cf7
Ralph Cowling <ralph@ralph-cowling.com>
2023-05-03 17:21:01 +0200

    include ukhomeoffice in jest transform ignore list

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testURL: 'http://localhost/',
   moduleDirectories: ['pages/common', 'node_modules', 'lib'],
   transformIgnorePatterns: [
-    'node_modules/(?!@asl)'
+    'node_modules/(?!(@ukhomeoffice|@asl))'
   ],
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
   snapshotSerializers: ['enzyme-to-json/serializer']
