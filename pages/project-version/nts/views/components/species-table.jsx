e0cd379282393a923dac27919003cd848f0a9812
Jeff Horton <87995501+jeff-horton-ho-sas@users.noreply.github.com>
2024-12-09 16:47:24 +0000

    [ASL-4647] Fix NTS Errors when a protocol has a species without details (#1664)

--- pages/project-version/nts/views/components/species-table.jsx
+++ pages/project-version/nts/views/components/species-table.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import uniq from 'lodash/uniq';
 import concat from 'lodash/concat';
 
+import schema from '@asl/projects/client/schema/v1/index';
+const lifeStageOptions = schema().protocols.subsections.protocols.sections.animals.fields.find(field => field.name === 'life-stages').options;
+
 const groupSpeciesDetails = version => {
   return (version.protocols || []).reduce((species, protocol) => {
     const activeDetails = (protocol?.species ?? [])
       .map(speciesKey => protocol?.speciesDetails?.find(d => (d.value ?? d.name) === speciesKey))
+      .filter(Boolean) // Species might not have details
       // Other species have "undefined" in value
       .map(details => ({...details, value: details.value ?? details.name}))
       .filter(Boolean);
@@ -20,14 +24,22 @@ const groupSpeciesDetails = version => {
         species.push({
           value: details.value,
           name: details.name,
-          lifeStages: details['life-stages'],
+          lifeStages: details['life-stages'] ?? [],
           maximumAnimals: parseInt(details['maximum-animals'], 10)
         });
       }
     });
 
     return species;
-  }, []);
+  }, [])
+    // Map to life stages to labels with a consistent order
+    .map(({lifeStages, ...rest}) => ({
+      ...rest,
+      lifeStages:
+      lifeStageOptions
+        .filter(({value}) => lifeStages.includes(value))
+        .map(({label}) => label)
+    }));
 };
 
 export default function SpeciesTable({ version }) {
